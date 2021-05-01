ae19384b8e4161b82c266aa23f1d5c4a232cbd0a
spartanCode <ochafloquet@gmail.com>
2021-05-01 16:43:10 -0500

    iniciando el proyecto web React

---
+++ src/peliculas/PeliculaIndividual.tsx
@@ -0,0 +1,13 @@
+import { pelicula } from "./peliculas.model";
+
+export default function PeliculaIndividual(props: peliculaIndividualProps){
+    return (
+        <div>
+            <a href="..."></a>
+        </div>
+    )
+}
+
+interface peliculaIndividualProps{
+    pelicula: pelicula;
+}
